e0bf97aef453dc052737f62691338d069e1c676a
lucien <lcpingping@gmail.com>
2016-10-07 00:13:24 -0700

    leetcode 230
    leetcode 230

--- C++/201-250/230_Kth Smallest Element in a BST.cpp
+++ C++/201-250/230_Kth Smallest Element in a BST.cpp
@@ -1,38 +1,12 @@
-#include <iostream>
-#include <bitset>
-#include <ctime>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
-#include <cctype> // For the isDigit
-#include <stack>
-#include <sstream> // For the change between int and string or any type
-//int a=10;
-//stringstream ss;
-//ss<<a;
-//return ss.str();
-
-// typedef long long ll;
-
-using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 private:
     int index;
@@ -41,24 +15,18 @@ public:
     int kthSmallest(TreeNode* root, int k) {
         index = 0;
         ans = 0;
-        solve(root, index, k);
+        solve(root, k);
         return ans;
     }
-    void solve(TreeNode* root, int index, int k) {
-        if (root == NULL) {
-            return;
-        } else if(index == k-1) {
-            ans = root->val;
-        } else {
-            solve(root->left, ++index, k);
-            if ()
+    void solve(TreeNode* root, int k) {
+        if (root == NULL) return;
+        else {
+            solve(root->left, k);
+            if (++index == k) {
+                ans = root->val;
+                return;
+            }
+            solve(root->right, k);
         }
     }
-    
 };
-
-int main(){
-    Solution a;
-
-    return 0;
-}
