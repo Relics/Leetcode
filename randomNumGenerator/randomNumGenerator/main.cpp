472bd59e1181f6136579926e682eceee8875b9ac
lucien <lcpingping@gmail.com>
2016-10-22 02:34:16 -0700

    randomNum
    randomNum

---
+++ randomNumGenerator/randomNumGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  randomNumGenerator
+//
+//  Created by Don‘t open when alone on 10/21/16.
+//  Copyright © 2016 Chen LI. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
